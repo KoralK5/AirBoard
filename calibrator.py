7f1e7b08cc8041901cbaa747a98232c9cf2c53b3
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-06-01 01:02:28 -0400

    Update calibrator.py

--- calibrator.py
+++ calibrator.py
@@ -3,13 +3,13 @@ import tracker
 import keyboard
 from os import system
 
-def calibrate(cap, detector):
+def calibrate(cap, detector, keyAmount):
 	system('cls')
 	print('CALIBRATING...\n\n')
 	print('Please center your keyboard under your camera and press every alphabet letter!\n')
 
-	n, coords, prevKey = 0, [], ''
-	while len(coords) < 26:
+	n, coords, prevKey = 0, [], []
+	while len(coords) < keyAmount:
 		n += 1
 		success, img = cap.read()
 		img = detector.findHands(img)
@@ -17,18 +17,18 @@ def calibrate(cap, detector):
 		if len(joints):
 			try:
 				key = keyboard.read_key()
-				if key != prevKey:
+				if key not in prevKey:
 					system('cls')
 					print(f'FRAME {n}:\n')
 					print(f'{key} at {joints[8]}')
 					coords.append((key, joints[8][1], joints[8][2]))
-				prevKey = key
+				prevKey += [key]
 			except:
 				pass
 			
 		cv2.imshow(f'Joint Detector', img)
 		cv2.waitKey(1)
-		
+
 	system('cls')
 	print('Calibration Sucessfull!')
 	return coords
