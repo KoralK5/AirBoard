c00bc3feee1a77f2c36a9f03f0e7177c6545d563
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-06-01 01:20:14 -0400

    Update airboard.py

--- airboard.py
+++ airboard.py
@@ -4,15 +4,13 @@ import tracker
 import cv2
 from os import system
 
+amount = int(input('Amount of keys to use: '))
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 detector = tracker.handDetector(maxHands=1)
-locations = cal.calibrate(cap, detector, 26)
-
-# locations = [('q', 545, 341), ('w', 547, 349), ('e', 528, 353), ('r', 503, 347), ('t', 480, 347), ('y', 452, 345), ('u', 425, 344), ('i', 398, 342), ('o', 373, 341), ('p', 348, 340), ('a', 323, 337), ('s', 546, 323), ('d', 514, 320), ('f', 498, 327), ('g', 473, 322), ('h', 447, 324), ('j', 420, 324), ('k', 392, 324), ('l', 369, 321), ('z', 338, 320), ('x', 528, 289), ('c', 503, 302), ('v', 481, 304), ('b', 459, 304), ('n', 424, 305), ('m', 403, 305)]
-
-print('\nKeyboard Coordinates:\n', locations)
+locations = cal.calibrate(cap, detector, amount)
 
 n = 0
 while True:
