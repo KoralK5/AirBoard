3fd0814fb48f8d79a6d703e90914bf15ee360586
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-05-31 20:10:08 -0400

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -1,6 +1,7 @@
 import cv2
 import tracker
 import numpy as np
+from os import system
 
 wCam, hCam = 640, 480
 
@@ -9,6 +10,7 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 detector = tracker.handDetector(maxHands=1)
 
+system('cls')
 while True:
 	success, img = cap.read()
 	img = detector.findHands(img)
@@ -16,11 +18,9 @@ while True:
 	coords = detector.results.multi_hand_landmarks
 	joints, bbox = detector.findPosition(img)
 	
-	print(len(joints))
 	if len(joints) != 0:
-		x1, y1 = joints[8][1:]
-		x2, y2 = joints[12][1:]
-		print(x1, y1, x2, y2)
+		system('cls')
+		print('Joints:\n', np.array(joints))
 	
 	cv2.imshow("Image", img)
 	cv2.waitKey(1)
