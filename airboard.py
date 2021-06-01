409d77ba54ec6ac5ff44b7bd973d849bd4e22370
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-06-01 02:52:39 -0400

    Update airboard.py

--- airboard.py
+++ airboard.py
@@ -3,6 +3,7 @@ import detector as det
 import tracker
 import cv2
 from os import system
+from time import time
 
 amount = int(input('Amount of keys to use: '))
 
@@ -12,6 +13,7 @@ cap.set(4, 480)
 detector = tracker.handDetector(maxHands=1)
 locations = cal.calibrate(cap, detector, amount)
 
+prevKey, start = '', time()
 while True:
 	coords, img, bbox = det.scan(cap, detector)
 
@@ -20,7 +22,18 @@ while True:
 		key, best = det.closest(locations, x, y)
 
 		xmin, ymin, xmax, ymax = bbox
-		cv2.putText(img, key.upper(), ((xmin+xmax)//2, ymin-50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2, cv2.LINE_AA)
+
+		if prevKey != key:
+			start = time()
+
+		if time() - start > 1:
+			cv2.putText(img, key.upper(), ((xmin+xmax)//2, ymin-50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,255), 2, cv2.LINE_AA)
+			cv2.rectangle(img, (xmin-20, ymin-20), (xmax+20, ymax+20), (0,255,255), 2)
+
+		else:
+			cv2.putText(img, key.upper(), ((xmin+xmax)//2, ymin-50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2, cv2.LINE_AA)
+		
+		prevKey = key
 
 	cv2.imshow(f'Keypress Detector', img)
 	cv2.waitKey(1)
